44a4a3e0f5588d85651835b2e6239fba3cc78f4b
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2023-07-06 11:32:29 +0800

    Basic feature completed

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 class TrieNode
 {
@@ -40,20 +42,14 @@ class Trie
 
     public bool Search(string word)
     {
-        TrieNode node = FindNode(word);
+        TrieNode node = StartsWith(word);
         return node != null && node.IsEndOfWord;
     }
 
-    public bool StartsWith(string prefix)
-    {
-        TrieNode node = FindNode(prefix);
-        return node != null;
-    }
-
-    private TrieNode FindNode(string str)
+    public TrieNode StartsWith(string prefix)
     {
         TrieNode current = root;
-        foreach (char c in str)
+        foreach (char c in prefix)
         {
             if (current.Children.ContainsKey(c))
             {
@@ -66,13 +62,54 @@ class Trie
         }
         return current;
     }
+
+    public List<string> AutoComplete(string prefix)
+    {
+        TrieNode node = StartsWith(prefix);
+        List<string> suggestions = new List<string>();
+        
+        if (node == null)
+        {
+            return suggestions;
+        }
+
+        CollectWords(node, prefix, suggestions);
+
+        return suggestions;
+    }
+
+    void CollectWords(TrieNode node, string prefix, List<string> suggestions)
+    {
+        if (node.IsEndOfWord)
+        {
+            suggestions.Add(prefix);
+        }
+
+        foreach (TrieNode childNode in node.Children.Values)
+        {
+            CollectWords(childNode, prefix + childNode.Value, suggestions);
+        }
+    }
+
+    public string ToJson()
+    {
+        return JsonConvert.SerializeObject(root, Formatting.Indented);
+    }
+
+    public static Trie FromJson(string json)
+    {
+        TrieNode root = JsonConvert.DeserializeObject<TrieNode>(json);
+        Trie trie = new Trie();
+        trie.root = root;
+        return trie;
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        Trie trie = new();
+        Trie trie = new Trie();
 
         // 插入詞彙到 Trie
         trie.Insert("apple");
@@ -80,18 +117,32 @@ class Program
         trie.Insert("banana");
         trie.Insert("car");
 
-        // 搜尋詞彙
-        Console.WriteLine(trie.Search("apple"));   // true
-        Console.WriteLine(trie.Search("app"));     // true
-        Console.WriteLine(trie.Search("appl"));    // false
-        Console.WriteLine(trie.Search("banana"));  // true
-        Console.WriteLine(trie.Search("car"));     // true
-        Console.WriteLine(trie.Search("cat"));     // false
+        // 將 Trie 轉換成 JSON 字串
+        string json = trie.ToJson();
 
-        // 判斷是否以指定字首開頭
-        Console.WriteLine(trie.StartsWith("ap"));  // true
-        Console.WriteLine(trie.StartsWith("ban")); // true
-        Console.WriteLine(trie.StartsWith("ca"));  // true
-        Console.WriteLine(trie.StartsWith("dog")); // false
+        // 寫入 JSON 檔案
+        System.IO.File.WriteAllText("lexicon.json", json);
+
+        // // 從 JSON 檔案讀取 Trie
+        // string jsonText = System.IO.File.ReadAllText("path/to/your/file.json");
+        // Trie newTrie = Trie.FromJson(jsonText);
+
+        // 自動完成
+        Console.Write("請輸入字首：");
+        string input = Console.ReadLine();
+        List<string> suggestions = trie.AutoComplete(input);
+
+        if (suggestions.Count == 0)
+        {
+            Console.WriteLine("無建議詞彙。");
+        }
+        else
+        {
+            Console.WriteLine("建議詞彙：");
+            foreach (string suggestion in suggestions)
+            {
+                Console.WriteLine(suggestion);
+            }
+        }
     }
 }
