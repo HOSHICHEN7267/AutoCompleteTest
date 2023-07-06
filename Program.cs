86e533e90fe1a3696beeb62e69d227bbe8a61653
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2023-07-06 13:40:14 +0800

    Json lexicon completed

--- Program.cs
+++ Program.cs
@@ -109,13 +109,29 @@ class Program
 {
     static void Main()
     {
+        string lexiconFile = "lexicon.txt";
+
         Trie trie = new Trie();
 
-        // 插入詞彙到 Trie
-        trie.Insert("apple");
-        trie.Insert("app");
-        trie.Insert("banana");
-        trie.Insert("car");
+        try
+        {
+            using (StreamReader sr = new StreamReader(lexiconFile))
+            {
+                string line;
+                // for(int i = 0 ; i < 10 ; ++i){
+                //     line = sr.ReadLine();
+                //     trie.Insert(line);
+                // }
+                while ((line = sr.ReadLine()) != null)
+                {
+                    trie.Insert(line);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
 
         // 將 Trie 轉換成 JSON 字串
         string json = trie.ToJson();
@@ -123,26 +139,27 @@ class Program
         // 寫入 JSON 檔案
         System.IO.File.WriteAllText("lexicon.json", json);
 
+
         // // 從 JSON 檔案讀取 Trie
         // string jsonText = System.IO.File.ReadAllText("path/to/your/file.json");
         // Trie newTrie = Trie.FromJson(jsonText);
 
-        // 自動完成
-        Console.Write("請輸入字首：");
-        string input = Console.ReadLine();
-        List<string> suggestions = trie.AutoComplete(input);
+        // // 自動完成
+        // Console.Write("請輸入字首：");
+        // string input = Console.ReadLine();
+        // List<string> suggestions = trie.AutoComplete(input);
 
-        if (suggestions.Count == 0)
-        {
-            Console.WriteLine("無建議詞彙。");
-        }
-        else
-        {
-            Console.WriteLine("建議詞彙：");
-            foreach (string suggestion in suggestions)
-            {
-                Console.WriteLine(suggestion);
-            }
-        }
+        // if (suggestions.Count == 0)
+        // {
+        //     Console.WriteLine("無建議詞彙。");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("建議詞彙：");
+        //     foreach (string suggestion in suggestions)
+        //     {
+        //         Console.WriteLine(suggestion);
+        //     }
+        // }
     }
 }
